9f57a23e0a33006213b1e3f541032e26aa83e35d
Luis Burgos <luisburgos@users.noreply.github.com>
2025-08-20 21:50:44 -0600

    chore: add simple main test file for coverage

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,14 +1,10 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:example/shared/workbench/bloc/workbench_bloc.dart';
+import 'package:example/shared/workbench/domain.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Dummy test', (WidgetTester tester) async {
-    expect(true, isTrue);
+  test('initial visible panes is empty', () {
+    final bloc = WorkbenchBloc(const WorkbenchState());
+    expect(bloc.state.visiblePanes, {WorkbenchPane.left});
   });
 }
